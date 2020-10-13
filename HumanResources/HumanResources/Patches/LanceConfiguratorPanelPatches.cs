d6176c62dfb554275e39547f5fe95a5db7642b31
Gerrit Kitts <gkitts@gmail.com>
2020-10-13 17:11:38 -0400

    - Non mechwarrior/vehicle crews are filtered from lance configuration

--- HumanResources/HumanResources/Patches/LanceConfiguratorPanelPatches.cs
+++ HumanResources/HumanResources/Patches/LanceConfiguratorPanelPatches.cs
@@ -1,21 +1,28 @@
 ﻿using BattleTech;
 using BattleTech.UI;
+using Harmony;
 using HumanResources.Extensions;
 using System.Collections.Generic;
 
 namespace HumanResources.Patches
 {
+    [HarmonyPatch(typeof(LanceConfiguratorPanel), "SetData")]
     static class LanceConfiguratorPanel_SetData
     {
-        static void Prefix(LanceConfiguratorPanel __instance, List<Pilot> pilots)
+        static void Prefix(LanceConfiguratorPanel __instance, ref List<Pilot> pilots)
         {
             // Remove any pilots who are aerospace, mechtechs, or medtechs
+            Mod.Log.Debug?.Write("Filtering pilots for LanceConfiguratorPanel");
             List<Pilot> selectablePilots = new List<Pilot>();
             foreach (Pilot p in pilots)
             {
                 CrewDetails details = new CrewDetails(p.pilotDef);
                 if (details.IsMechWarrior || details.IsVehicleCrew)
+                {
+                    Mod.Log.Debug?.Write($"Pilot {p.Name} is a mechwarrior or vehicle crew, adding as option");
                     selectablePilots.Add(p);
+                }
+                    
             }
 
             pilots.Clear();
