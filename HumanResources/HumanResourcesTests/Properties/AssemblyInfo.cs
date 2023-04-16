5d66140dd8d9f65ae4c3309a906695fa687fc408
IceRaptor <gkitts@gmail.com>
2023-04-16 11:12:45 -0400

    - Update to MS 2019 build

--- HumanResources/HumanResourcesTests/Properties/AssemblyInfo.cs
+++ HumanResources/HumanResourcesTests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("HumanResourcesTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HumanResourcesTests")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("1fddd2d0-690c-478b-a623-d8ef590f098e")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
